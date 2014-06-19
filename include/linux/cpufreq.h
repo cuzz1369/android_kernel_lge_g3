6747d13bfc91385ed74649e101cdfbbedf6bba5e
TroNit <ttronit@gmail.com>
2014-06-19 18:53:17 -0400

    Optimizations, trying to get 4.8
    Conflicts:
    	arch/arm/configs/jet_defconfig
    
    Conflicts:
    	arch/arm/configs/jet_defconfig
    	include/linux/cpufreq.h

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -399,7 +399,6 @@ extern struct cpufreq_governor cpufreq_gov_ondemandplus;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIACTIVE)
 extern struct cpufreq_governor cpufreq_gov_intelliactive;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intelliactive)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIACTIVE)
 #endif
 
 

a3bad64dc922782bfa2cf962d667328b087a6ac3
yank555-lu <yank555.lu@gmail.com>
2014-09-09 20:51:39 +0200

    msm: mdss: Display gamma control (myfluxi, faux123)
    msm: mdss: Display gamma control
    
    TODO: confirm rgb channels, identify other tunables, cleanup,
    apply values without switching screen (off and) on.
    
    sysfs: /sys/devices/qcom,mdss_dsi_jdi_command_ors.69/*
    msm: mdss: Apply commands values without switching screen (off and) on
    msm: mdss: Remove tunable for command 32
    
    Even slight changes look ugly or distort the screen.
    msm: mdss: distinguish between Gamma RGB polarity Positive/Negative
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    msm: mdss: Remove sysfs for unused commands
    msm: mdss: Identify white point in sysfs
    msm: mdss: Add safety check for user input
    
    Leave more checks either to the control app or the user's intelligence.
    msm: mdss: Finalize gamma interface and cleanup
    
    - Remove all sysfs files of unused commands
    - Remove auto sync for negative polarities, these must be written separately
    - Protect offset bit of white point command as well
    - Apply white point bit 2 as duplicate of bit 1
    msm: mdss: Send panel commands via separate function
    
    Sending panel comnmands is very expensive, add a separate function to
    accomplish this. Once all commands have been stored write anything to
    the kgamma_send file.
    msm: mdss: Add hardware revision check
    
    Our gamma interface works with both current hardware revisions,
    11 and 11j, although all testing was carried out on 11. As we
    don't know about possible future revisions, let's bail out until
    correct offsets for panel commands have been added.
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    msm: mdss: Use workqueue to send panel commands
    
    Sending panel commands is known to be expensive, honor this by
    scheduling this task. Also check for panel state and send commands
    only when we're actually awake.
    
    These fixes adress bug reports regarding behaviour during boot.
    msm: mdss: Find a stable sysfs node for the gamma interface
    
    Move sysfs files to /sys/module/dsi_panel/*
    msm: mdss: Reset panel to apply RGB gamma data
    
    Writing to the reset gpios produces a short flicker. This however
    appears to be the only reliable method to apply RGB gamma data
    without switching the panel off and on again.
    
    Conflicts:
    	drivers/video/msm/mdss/mdss_dsi.c
    	drivers/video/msm/mdss/mdss_dsi_panel.c

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -22,10 +22,14 @@
 #include <linux/qpnp/pwm.h>
 #include <linux/err.h>
 
+#include <asm/system_info.h>
+
 #include "mdss_dsi.h"
 #include <mach/board_lge.h>
 
 #define DT_CMD_HDR 6
+
+#include "mdss_mdp.h"
 
 #ifdef CONFIG_MACH_LGE
 extern struct platform_device *of_find_device_by_node(struct device_node *np);
@@ -41,6 +45,8 @@ extern int num_cmds;
 extern struct dsi_cmd_desc *tun_dsi_panel_on_cmds;
 static int num_of_on_cmds;
 #endif
+#define DT_CMD_HDR 6
+#define GAMMA_COMPAT 11
 
 #ifdef CONFIG_MFD_TPS65132
 extern int tps65132_regulate_voltage(int on, int mode);
@@ -59,6 +65,14 @@ extern void lm3697_lcd_backlight_set_level(int level);
 extern void lm3631_lcd_backlight_set_level(int level);
 #endif
 #endif
+extern void mdss_mdp_cmds_send(unsigned int on);
+
+static struct mdss_dsi_phy_ctrl phy_params;
+static struct mdss_panel_common_pdata *local_pdata;
+static struct work_struct send_cmds_work;
+struct mdss_panel_data *cmds_panel_data;
+static struct platform_driver this_driver;
+struct kobject *module_kobj;
 
 void mdss_dsi_panel_pwm_cfg(struct mdss_dsi_ctrl_pdata *ctrl)
 {
@@ -547,6 +561,9 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 #else
 		mdss_dsi_panel_cmds_send(ctrl, &ctrl->on_cmds);
 #endif
+
+	if (local_pdata->on_cmds.cmd_cnt)
+		mdss_dsi_panel_cmds_send(ctrl, &local_pdata->on_cmds);
 
 	pr_debug("%s:\n", __func__);
 	return 0;
@@ -1363,4 +1380,256 @@ int mdss_dsi_panel_init(struct device_node *node,
 	rc = device_create_file(&ctrl_pdev->dev, &dev_attr_ief_on_off);
 #endif
 	return 0;
+
+static int read_local_on_cmds(char *buf, size_t cmd)
+{
+	int i, len = 0;
+	int dlen;
+
+	if (system_rev != GAMMA_COMPAT) {
+		pr_err("Incompatible hardware revision: %d\n", system_rev);
+		return -EINVAL;
+	}
+
+	dlen = local_pdata->on_cmds.cmds[cmd].dchdr.dlen - 1;
+	if (!dlen)
+		return -ENOMEM;
+
+	/* don't print first and last bits */
+	for (i = 1; i < dlen; i++)
+		len += sprintf(buf + len, "%d ",
+			       local_pdata->on_cmds.cmds[cmd].payload[i]);
+
+	len += sprintf(buf + len, "\n");
+
+	return len;
+}
+
+static unsigned int cnt;
+
+static int write_local_on_cmds(struct device *dev, const char *buf,
+			       size_t cmd)
+{
+	int i, rc = 0;
+	int dlen;
+	unsigned int val;
+	char tmp[3];
+	struct mdss_dsi_ctrl_pdata *ctrl = NULL;
+	struct mdss_panel_common_pdata *prev_local_data;
+
+	if (cnt) {
+		cnt = 0;
+		return -EINVAL;
+	}
+
+	if (system_rev != GAMMA_COMPAT) {
+		pr_err("Incompatible hardware revision: %d\n", system_rev);
+		return -EINVAL;
+	}
+
+	if (cmds_panel_data == NULL) {
+		pr_err("%s: Invalid input data\n", __func__);
+		return -EINVAL;
+	}
+
+	ctrl = container_of(cmds_panel_data, struct mdss_dsi_ctrl_pdata,
+				panel_data);
+
+	/*
+	 * Last bit is not written because it's either fixed at 0x00 for
+	 * RGB or a duplicate of the previous bit for the white point.
+	 */
+	dlen = local_pdata->on_cmds.cmds[cmd].dchdr.dlen - 1;
+	if (!dlen)
+		return -EINVAL;
+
+	prev_local_data = local_pdata;
+
+	for (i = 1; i < dlen; i++) {
+		rc = sscanf(buf, "%u", &val);
+		if (rc != 1)
+			return -EINVAL;
+
+		if (val > 255) {
+			pr_err("%s: Invalid input data %u (0-255)\n", __func__, val);
+			local_pdata = prev_local_data;
+			return -EINVAL;
+		}
+
+		local_pdata->on_cmds.cmds[cmd].payload[i] = val;
+		/* white point value must be duplicated */
+		if (cmd == 5)
+			local_pdata->on_cmds.cmds[cmd].payload[i + 1] = val;
+
+		sscanf(buf, "%s", tmp);
+		buf += strlen(tmp) + 1;
+		cnt = strlen(tmp);
+	}
+
+	pr_info("%s\n", __func__);
+
+	return rc;
+}
+
+static void send_local_on_cmds(struct work_struct *work)
+{
+	struct mdss_dsi_ctrl_pdata *ctrl = NULL;
+
+	if (cmds_panel_data == NULL) {
+		pr_err("%s: Invalid input data\n", __func__);
+		return;
+	}
+
+	ctrl = container_of(cmds_panel_data, struct mdss_dsi_ctrl_pdata,
+			    panel_data);
+
+	/* Prevent flicker during continous splash */
+	if (cmds_panel_data->panel_info.cont_splash_enabled)
+		return;
+
+	mdss_mdp_cmds_send(1);
+	gpio_set_value((ctrl->rst_gpio), 0);
+	udelay(200);
+	gpio_set_value((ctrl->rst_gpio), 1);
+	msleep(20);
+
+	if (ctrl->ctrl_state & CTRL_STATE_PANEL_INIT)
+		ctrl->ctrl_state &= ~CTRL_STATE_PANEL_INIT;
+
+	if (local_pdata->on_cmds.cmd_cnt)
+		mdss_dsi_panel_cmds_send(ctrl, &local_pdata->on_cmds);
+	mdss_mdp_cmds_send(0);
+
+	pr_info("%s\n", __func__);
+}
+
+/************************** sysfs interface ************************/
+
+static ssize_t write_kgamma_send(struct device *dev,
+				 struct device_attribute *attr,
+				 const char *buf, size_t count)
+{
+	if (!cmds_panel_data->panel_info.panel_power_on) {
+		pr_err("%s: Panel off, failed to send commands\n", __func__);
+		return -EPERM;
+	}
+
+	schedule_work(&send_cmds_work);
+
+	return count;
+}
+
+static DEVICE_ATTR(kgamma_send, 0644, NULL, write_kgamma_send);
+
+#define read_one(file_name, cmd)				\
+static ssize_t read_##file_name					\
+(struct device *dev, struct device_attribute *attr, char *buf)  \
+{								\
+	return read_local_on_cmds(buf, cmd);			\
+}
+
+read_one(kgamma_w,   5);
+read_one(kgamma_rp,  7);
+read_one(kgamma_rn,  9);
+read_one(kgamma_gp, 11);
+read_one(kgamma_gn, 13);
+read_one(kgamma_bp, 15);
+read_one(kgamma_bn, 17);
+
+#define write_one(file_name, cmd)				\
+static ssize_t write_##file_name				\
+(struct device *dev, struct device_attribute *attr, 		\
+		const char *buf, size_t count)  		\
+{								\
+	return write_local_on_cmds(dev, buf, cmd);		\
+}
+
+write_one(kgamma_w,   5);
+write_one(kgamma_rp,  7);
+write_one(kgamma_rn,  9);
+write_one(kgamma_gp, 11);
+write_one(kgamma_gn, 13);
+write_one(kgamma_bp, 15);
+write_one(kgamma_bn, 17);
+
+#define define_one_rw(_name)					\
+static DEVICE_ATTR(_name, 0644, read_##_name, write_##_name);
+
+define_one_rw(kgamma_w);
+define_one_rw(kgamma_rp);
+define_one_rw(kgamma_rn);
+define_one_rw(kgamma_gp);
+define_one_rw(kgamma_gn);
+define_one_rw(kgamma_bp);
+define_one_rw(kgamma_bn);
+
+static struct attribute *dsi_panel_attributes[] = {
+	&dev_attr_kgamma_w.attr,
+	&dev_attr_kgamma_rp.attr,
+	&dev_attr_kgamma_rn.attr,
+	&dev_attr_kgamma_gp.attr,
+	&dev_attr_kgamma_gn.attr,
+	&dev_attr_kgamma_bp.attr,
+	&dev_attr_kgamma_bn.attr,
+	&dev_attr_kgamma_send.attr,
+	NULL
+};
+
+static struct attribute_group dsi_panel_attribute_group = {
+	.attrs = dsi_panel_attributes,
+};
+
+/**************************** sysfs end **************************/
+
+static int __devinit mdss_dsi_panel_probe(struct platform_device *pdev)
+{
+	int rc = 0;
+	static struct mdss_panel_common_pdata vendor_pdata;
+	static const char *panel_name;
+	const char *driver_name = this_driver.driver.name;
+
+	pr_debug("%s:%d, debug info id=%d", __func__, __LINE__, pdev->id);
+	if (!pdev->dev.of_node)
+		return -ENODEV;
+
+	panel_name = of_get_property(pdev->dev.of_node, "label", NULL);
+	if (!panel_name)
+		pr_info("%s:%d, panel name not specified\n",
+						__func__, __LINE__);
+	else
+		pr_info("%s: Panel Name = %s\n", __func__, panel_name);
+
+	rc = mdss_panel_parse_dt(pdev, &vendor_pdata);
+	if (rc)
+		return rc;
+
+	vendor_pdata.on = mdss_dsi_panel_on;
+	vendor_pdata.off = mdss_dsi_panel_off;
+	vendor_pdata.bl_fnc = mdss_dsi_panel_bl_ctrl;
+
+	rc = dsi_panel_device_register(pdev, &vendor_pdata);
+	if (rc)
+		return rc;
+
+	INIT_WORK(&send_cmds_work, send_local_on_cmds);
+
+	local_pdata = &vendor_pdata;
+	if (!local_pdata)
+		return -EINVAL;
+
+#ifdef CONFIG_DEBUG_FS
+	debug_fs_init(&vendor_pdata);
+#endif
+
+	module_kobj = kobject_create_and_add(driver_name, &module_kset->kobj);
+	if (!module_kobj) {
+		pr_err("%s: kobject create failed\n", driver_name);
+		return -ENOMEM;
+	}
+
+	rc = sysfs_create_group(module_kobj, &dsi_panel_attribute_group);
+	if (rc)
+		pr_err("%s: sysfs create failed: %d\n", panel_name, rc);
+
+	return rc;
 }
